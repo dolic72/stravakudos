79943c720f948012072dea329e28eab9a378f188
dolic72 <dubravko@dolic.de>
2020-12-12 17:47:49 +0100

    Write to db

--- read_full_activity_data.py
+++ read_full_activity_data.py
@@ -6,6 +6,8 @@ import numpy as np
 import json
 import csv
 import requests
+import psycopg2
+import psycopg2.extras as extras
 from stravalib.client import Client
 
 client = Client()
@@ -105,6 +107,76 @@ def get_gear_names(activity_meta):
         df_ret = pd.concat([df_ret, df], axis=0)
     return df_ret
 
+# store database credentials
+with open('.secret/postgres_credentials.json', 'r') as f:
+    postgres_credentials = json.load(f)
+    host = postgres_credentials['host']
+    database = postgres_credentials['database']
+    user = postgres_credentials['user']
+    password = postgres_credentials['password']
+
+# open connection to database
+conn = psycopg2.connect(host=host, database=database, user=user, password=password)# create cursor object
+cur = conn.cursor()
+
+# write SQL query
+create_table_query = """
+CREATE TABLE activities (
+    id BIGINT PRIMARY KEY,
+    name CHARACTER VARYING(255) NOT NULL,
+    start_date_local CHARACTER(64) NOT NULL,
+    start_date CHARACTER(64) NOT NULL,
+    type CHARACTER VARYING(64) NOT NULL,
+    distance NUMERIC NOT NULL,
+    moving_time CHARACTER VARYING(10) NOT NULL,
+    elapsed_time CHARACTER VARYING(10) NOT NULL,
+    total_elevation_gain NUMERIC,
+    elev_high NUMERIC,
+    elev_low NUMERIC,
+    average_speed NUMERIC,
+    max_speed NUMERIC,
+    average_heartrate NUMERIC,
+    max_heartrate NUMERIC,
+    start_latitude NUMERIC,
+    start_longitude NUMERIC,
+    kudos_count SMALLINT,
+    average_temp CHARACTER VARYING(10),
+    has_heartrate CHARACTER VARYING(10),
+    calories CHARACTER VARYING(10),
+    gear_id CHARACTER VARYING(10)
+);
+"""
+
+# commit table to database
+cur.execute(create_table_query)
+conn.commit()
+
+def execute_batch(conn, df, table, page_size=100):
+    """
+    Using psycopg2.extras.execute_batch() to insert the dataframe
+    """
+    # Create a list of tupples from the dataframe values
+    tuples = [tuple(x) for x in df.to_numpy()]
+    # Comma-separated dataframe columns
+    cols = ','.join(list(df.columns))
+    # SQL query to execute
+    query  = "INSERT INTO %s(%s) VALUES(%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s,%%s)" % (table, cols)
+    cursor = conn.cursor()
+    try:
+        extras.execute_batch(cursor, query, tuples, page_size)
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("execute_batch() done")
+    cursor.close()    
 
 
-my_activities = get_activities("2020-12-01 00:00:00")
+
+my_activities = get_activities("2020-10-01 00:00:00")
+execute_batch(conn, my_activities, "activities")
+
+# close connection to database
+conn.close()
